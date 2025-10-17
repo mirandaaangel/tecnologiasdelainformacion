628362f4129f8473b671eea1edc5ff35ac3b70f5
mirandaaangel <mirandaramirezangelantonio@gmail.com>
2025-10-17 07:46:28 -0600

    practica20

---
+++ practica20/js/index.js
@@ -0,0 +1,23 @@
+let nombre = "Angel";
+let apellido = 'Miranda Ramirez'
+let nombrecompleto = nombre + ' ' + apellido;
+console.log(nombrecompleto)
+
+let numero = 250;
+let numero2 =300;
+var numero3 =100;
+numero4 =50; //declaraciones antiguas
+
+console.log(numero3, numero4);
+
+let suma = numero + numero2 + numero3 + numero4;
+console.log(suma);
+
+let resta = numero3 - numero2;
+console.log('El resultado es: ', resta);
+
+let multiplica = numero * numero2;
+console.log('El resultado es: ', multiplica);
+
+let divide = numero3 / numero4;
+console.log('El resultado de la división es: ', divide);
